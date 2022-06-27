3610c1cb6fe7189549ea21f00b2e8e8bd35f0a91
jae <wogud1514@naver.com>
2022-06-27 21:29:38 +0900

    시작

---
+++ src/main/java/jjfactory/learnmanage/LearnManageApplication.java
@@ -0,0 +1,13 @@
+package jjfactory.learnmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnManageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnManageApplication.class, args);
+	}
+
+}
